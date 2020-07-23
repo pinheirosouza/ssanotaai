4b05cc262da3f9a11e79719723f71d346fa379ab
Pedro Pinheiro <pinheiro@furg.br>
2020-07-23 18:25:58 -0300

    gitignore

--- views/src/app/app-routing.module.ts
+++ views/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VideoDemoComponent } from './self-service/video-demo/video-demo.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'video-demo',
+    component: VideoDemoComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'video-demo',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ views/src/app/self-service/video-demo/video-demo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-video-demo',
+  templateUrl: './video-demo.component.html',
+  styleUrls: ['./video-demo.component.scss']
+})
+export class VideoDemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
