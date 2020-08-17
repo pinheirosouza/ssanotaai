1aa63e136535b3350fa0a37e0dcc8d9d8fab869f
Pedro Pinheiro <pinheiro@furg.br>
2020-08-17 20:12:01 -0300

    mudando server

--- views/server.js
+++ views/server.js
@@ -3,12 +3,13 @@ const http = require('http');
 const path = require('path');
 
 const app = express();
-
 const port = process.env.PORT || 3000;
+console.log("Tentando rodar esa parada na porta ", port)
 
 app.use(express.static(__dirname + '/dist/ssanotaai'));
 
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname)));
+
 
 const server = http.createServer(app);
 
