852627221590de3d93f6a84a597d1813096d6764
Pedro Pinheiro <pinheiro@furg.br>
2020-08-18 10:56:10 -0300

    bla 8

--- views/src/environments/environment.prod.ts
+++ views/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url: 'http://ssanotaai.herokuapp.com',
+  url: 'https://staging-ssanotaai-api.herokuapp.com',
 };
--- views/src/environments/environment.ts
+++ views/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  url: 'http://staging-ssanotaai-api.herokuapp.com',
+  url: 'https://staging-ssanotaai-api.herokuapp.com',
 };
 
 /*
