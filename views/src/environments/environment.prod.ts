ba47678ed4c37772d8db0f1fff50ae460447556a
Pedro Pinheiro <pinheiro@furg.br>
2020-08-24 11:00:56 -0300

    env.prod

--- views/src/environments/environment.prod.ts
+++ views/src/environments/environment.prod.ts
@@ -1,4 +1,16 @@
 export const environment = {
   production: true,
+  videoUrl: 'https://ssanotaai.s3-sa-east-1.amazonaws.com/demo_1.mp4',
   url: 'https://staging-ssanotaai-api.herokuapp.com',
+  // url: 'http://localhost:3000',
+  moskit: 'https://moskit.herokuapp.com/auth/moskit',
+  firebase: {
+    apiKey: 'AIzaSyATcmFVuIq51Csk75VcQLWPdO-1FgOQSMY',
+    authDomain: 'vendas-anotaai.firebaseapp.com',
+    databaseURL: 'https://vendas-anotaai.firebaseio.com',
+    projectId: 'vendas-anotaai',
+    storageBucket: 'vendas-anotaai.appspot.com',
+    messagingSenderId: '635630891816',
+    appId: '1:635630891816:web:0f4c7913b608a8ff',
+  },
 };
